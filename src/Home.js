8d6f1bd908a5fa920044a1f38285c3d31e0dbf9c
noa <aggarwal.neeraj141@gmail.com>
2020-10-08 16:18:37 -0700

    Add dropdown for switch

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
-import { Input } from "antd";
+import { Button, Dropdown, Input, Menu } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import Synonyms from "./Synonyms";
 import Translations from "./Translations";
 const { Search } = Input;
 
 function Home() {
   const [word, setWord] = useState("");
+  const [find, setFind] = useState("Synonyms");
+
+  const onClick = ({ key }) => {
+    setFind(key);
+  };
+
+  const menu = (
+    <Menu onClick={onClick}>
+      <Menu.Item key="Synonyms">Synonyms</Menu.Item>
+      <Menu.Item key="Translations">Translations</Menu.Item>
+    </Menu>
+  );
 
   return (
     <>
@@ -14,8 +27,13 @@ function Home() {
         onSearch={(value) => setWord(value)}
         style={{ width: 400 }}
       />
-      <Translations word={word} />
-      <Synonyms word={word} />
+      <Dropdown overlay={menu}>
+        <Button>
+          {find} <DownOutlined />
+        </Button>
+      </Dropdown>
+      {word && find === "Translations" && <Translations word={word} />}
+      {word && find === "Synonyms" && <Synonyms word={word} />}
     </>
   );
 }
