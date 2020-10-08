b9bb26c13fc3f454859bf16682caf50d76cf2aa6
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-10-08 01:12:30 +0000

    Display results in grid (#2)
    * Start styling
    
    * Use word grid for translations

---
+++ src/WordCardGrid.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Col, Row } from "antd";
+import WordCard from "./WordCard";
+
+function WordCardGrid(props) {
+  const { words } = props;
+
+  const cards = Array(Math.ceil(words.length / 3))
+    .fill()
+    .map((_, index) => {
+      const first = words[index * 3];
+      const second = index * 3 + 1 < words.length ? words[index * 3 + 1] : null;
+      const third = index * 3 + 2 < words.length ? words[index * 3 + 2] : null;
+
+      return (
+        <Row gutter={[16, 16]}>
+          <Col span={8}>
+            <WordCard word={first} />
+          </Col>
+          {second && (
+            <Col span={8}>
+              <WordCard word={second} />
+            </Col>
+          )}
+          {third && (
+            <Col span={8}>
+              <WordCard word={third} />
+            </Col>
+          )}
+        </Row>
+      );
+    });
+
+  return <div className="resources-grid">{cards}</div>;
+}
+
+export default WordCardGrid;
