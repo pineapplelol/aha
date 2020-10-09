b53ca73740eb6812035d4a7f1f97c59dab28a6c6
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-10-09 19:25:45 +0000

    Improve styling (#4)
    * Center search box
    
    * Improve grid layout
    
    * Remove unecessary div in wordgrid

--- src/WordCard.js
+++ src/WordCard.js
@@ -3,7 +3,7 @@ import { Card } from "antd";
 
 function WordCard(props) {
   return (
-    <Card style={{ width: 300 }}>
+    <Card style={{ width: '90%' }}>
       <p>{props.word}</p>
     </Card>
   );
