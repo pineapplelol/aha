acb7da6ba4b80dbc0e2f598cd4963846e8ec706f
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-10-09 06:35:18 +0000

    Add toggle for translation and synonyms (#3)
    * Add dropdown for switch
    
    * Group dropdown and search
    
    * Upgrade dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
-import { Input } from "antd";
+import { Button, Dropdown, Input, Menu } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 import Synonyms from "./Synonyms";
 import Translations from "./Translations";
 const { Search } = Input;
 
 function Home() {
   const [word, setWord] = useState("");
+  const [find, setFind] = useState("Synonyms");
+
+  const onClick = ({ key }) => {
+    setFind(key);
+  };
+
+  const menu = (
+    <Menu onClick={onClick}>
+      <Menu.Item key="Synonyms">Synonyms</Menu.Item>
+      <Menu.Item key="Translations">Translations</Menu.Item>
+    </Menu>
+  );
 
   return (
     <>
-      <Search
-        placeholder="A word that describes your idea"
-        onSearch={(value) => setWord(value)}
-        style={{ width: 400 }}
-      />
-      <Translations word={word} />
-      <Synonyms word={word} />
+      <Input.Group>
+        <Dropdown overlay={menu}>
+          <Button>
+            {find} <DownOutlined />
+          </Button>
+        </Dropdown>
+        <Search
+          placeholder="A word that describes your idea"
+          onSearch={(value) => setWord(value)}
+          style={{ width: 400 }}
+        />
+      </Input.Group>
+      {word && find === "Translations" && <Translations word={word} />}
+      {word && find === "Synonyms" && <Synonyms word={word} />}
     </>
   );
 }
