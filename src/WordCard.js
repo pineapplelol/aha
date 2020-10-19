3273a50101ca980034ee8132b23eb82fb7999eb7
noa <aggarwal.neeraj141@gmail.com>
2020-10-19 14:23:37 -0700

    Start new card design

--- src/WordCard.js
+++ src/WordCard.js
@@ -3,8 +3,11 @@ import { Card } from "antd";
 
 function WordCard(props) {
   return (
-    <Card style={{ width: '90%' }}>
-      <p>{props.word}</p>
+    <Card style={{ width: "90%", textAlign: "left" }}>
+      <h2>{props.word}</h2>
+      <p>
+        <em>{props.identifier}</em> {props.description}
+      </p>
     </Card>
   );
 }
