596d54d313717055031e02ad90c8f62e6628faa4
noa <aggarwal.neeraj141@gmail.com>
2020-11-20 23:50:42 -0800

    Improve footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,20 +5,11 @@ import "../css/Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer-links">
-        <ul>
-          <li>
-            <a href="https://pineapple.lol/">About</a>
-          </li>
-          <li>
-            <a href="https://github.com/pineapplelol/venmo.lol">View Source</a>
-          </li>
-        </ul>
+        <div className="copyright">Copyright &#169; 2020 pineapplelol</div>
+        <a href="https://vercel.com?utm_source=pineapplelol&utm_campaign=oss">
+          <img src="/powered-by-vercel.svg" alt="vercel" />
+        </a>
       </div>
-      <a href="https://vercel.com?utm_source=pineapplelol&utm_campaign=oss">
-        <img src="/powered-by-vercel.svg" alt="vercel" className="vercel" />
-      </a>
-    </div>
   );
 }
 
