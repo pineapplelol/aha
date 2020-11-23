cce413b370a03bf80330c037f0bb0d709a1c8cc3
noa <aggarwal.neeraj141@gmail.com>
2020-11-23 14:48:31 -0800

    Improve card grid logic

--- src/components/WordCardGrid.js
+++ src/components/WordCardGrid.js
@@ -5,23 +5,19 @@ function WordCardGrid({ cards }) {
   const cardLayout = Array(Math.ceil(cards.length / 3))
     .fill()
     .map((_, i) => {
-      const first = cards[i * 3];
-      const second = i * 3 + 1 < cards.length ? cards[i * 3 + 1] : null;
-      const third = i * 3 + 2 < cards.length ? cards[i * 3 + 2] : null;
-
       return (
         <Row gutter={[16, 16]}>
           <Col xs={24} xl={8}>
-            {first}
+            {cards[i * 3]}
           </Col>
-          {second && (
+          {i * 3 + 1 < cards.length && (
             <Col xs={24} xl={8}>
-              {second}
+              {cards[i * 3 + 1]}
             </Col>
           )}
-          {third && (
+          {i * 3 + 2 < cards.length && (
             <Col xs={24} xl={8}>
-              {third}
+              {cards[i * 3 + 2]}
             </Col>
           )}
         </Row>
