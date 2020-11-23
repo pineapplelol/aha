b868147a6c44ee167be8c278261f5ef2cbfd59b4
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-11-23 03:00:50 +0000

    ☎️ Add mobile styling (#12)

---
+++ src/components/Searchbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button, Dropdown, Input, Menu, Row } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+
+import "../css/Searchbar.css";
+
+const { Search } = Input;
+
+function Searchbar({ setWord, find, setFind }) {
+  const onClick = ({ key }) => {
+    setFind(key);
+  };
+
+  const menu = (
+    <Menu onClick={onClick}>
+      <Menu.Item key="Synonyms">Synonyms</Menu.Item>
+      <Menu.Item key="Translations">Translations</Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <Row type="flex" align="center" className="search">
+      <Search
+        addonBefore={
+          <Dropdown overlay={menu}>
+            <Button>
+              {find} <DownOutlined />
+            </Button>
+          </Dropdown>
+        }
+        placeholder="A word that describes your idea"
+        onSearch={(value) => setWord(value)}
+        style={{ width: 400 }}
+      />
+    </Row>
+  );
+}
+
+export default Searchbar;
--- src/components/WordCardGrid.js
+++ src/components/WordCardGrid.js
@@ -4,23 +4,23 @@ import { Col, Row } from "antd";
 function WordCardGrid({ cards }) {
   const cardLayout = Array(Math.ceil(cards.length / 3))
     .fill()
-    .map((_, index) => {
-      const first = cards[index * 3];
-      const second = index * 3 + 1 < cards.length ? cards[index * 3 + 1] : null;
-      const third = index * 3 + 2 < cards.length ? cards[index * 3 + 2] : null;
+    .map((_, i) => {
+      const first = cards[i * 3];
+      const second = i * 3 + 1 < cards.length ? cards[i * 3 + 1] : null;
+      const third = i * 3 + 2 < cards.length ? cards[i * 3 + 2] : null;
 
       return (
         <Row gutter={[16, 16]}>
-          <Col span={8} align="middle">
+          <Col xs={24} xl={8}>
             {first}
           </Col>
           {second && (
-            <Col span={8} align="middle">
+            <Col xs={24} xl={8}>
               {second}
             </Col>
           )}
           {third && (
-            <Col span={8} align="middle">
+            <Col xs={24} xl={8}>
               {third}
             </Col>
           )}
